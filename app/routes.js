ae17c32422b8b9061f7ea8f65503ad1690bf49ee
Wells Tsai <wells.z.tsai@gmail.com>
2017-01-07 12:19:52 -0800

    Implement get maps function

--- app/routes.js
+++ app/routes.js
@@ -41,6 +41,7 @@ module.exports = function(app, passport) {
 
   app.post('/maps', isLoggedIn, function(req, res) {
     console.log('hit /post maps in server!');
+    console.log('req.session.passport.user:', req.session.passport.user);
     console.log(req.body);
     Maps.create({
       mapData: req.body.mapData,
@@ -51,11 +52,26 @@ module.exports = function(app, passport) {
     });
   })
 
+  app.get('/maps', isLoggedIn, function(req, res) {
+    var getMaps = {};
+    Maps.findAll({
+      where: {shareable: true}
+    }).then(function(publicMaps) {
+      getMaps[0] = publicMaps;
+      Maps.findAll({
+        where: {user_id: req.session.passport.user}
+      }).then(function(userMaps) {
+        getMaps[1] = userMaps;
+        res.send(getMaps);
+      })
+    });
+  });
+
   app.get('/logout', function(req, res) {
     req.logout();
     res.redirect('/');
-  });
-};
+    });
+  };
 
 
 
