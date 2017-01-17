63ca733da4350c9d9fe3eb0d937e9522a04c2855
Don Nguyen <nguyendk@gmail.com>
2017-01-16 20:17:46 -0800

    Refactored for webpack

--- client/components/router.jsx
+++ client/components/router.jsx
@@ -1,9 +1,11 @@
 // // Reactrouter was imported via cdn - defining often-used react-router variables here
-var Router = window.ReactRouter.Router;
-var Route = window.ReactRouter.Route;
-var Link = window.ReactRouter.Link;
-var browserHistory = window.ReactRouter.browserHistory;
-
+import {Router, Route, browserHistory, Link} from 'react-router';
+import LandingPage from './landingPage';
+import ProfilePage from './profilePage';
+import App from './app';
+import MazeStore from './store';
+import ReactDOM from 'react-dom';
+import React from 'react';
 
 // React router that switches between signin, signup, and pet app
 var MainRouter = () => (
@@ -11,6 +13,7 @@ var MainRouter = () => (
     <Route path="/" component={LandingPage} />
     <Route path="/profile" component={ProfilePage} />
     <Route path="/mazebuilder" component={App} />
+    <Route path="/mazestore" component={MazeStore} />
   </Router>
 );
 
