c2080a9527ac4c8086ad495ea368fb5a3bbb4afb
Wells Tsai <wells.z.tsai@gmail.com>
2017-01-16 17:50:41 -0800

    Add color theme, fonts, and other styles

--- client/components/signup.jsx
+++ client/components/signup.jsx
@@ -3,20 +3,20 @@ import React from 'react';
 var SignupForm = (props) => (
   <form id="signupForm">
     <div>
-      <label>Username:</label>
+      <label>Username: </label>
       <input type="text" name="username" id="signup-username"/>
     </div>
     <div>
-      <label>Email:</label>
+      <label>Email: </label>
       <input type="text" name="email"/>
     </div>
 
     <div>
-      <label>Password:</label>
+      <label>Password: </label>
       <input type="text" name="password"/>
     </div>
     <div>
-      <input type="submit" value="Sign Up" onClick={props.signupFormSubmit}/>
+      <input id="submitInput" type="submit" value="Sign Up" onClick={props.signupFormSubmit}/>
     </div>
   </form>
 
