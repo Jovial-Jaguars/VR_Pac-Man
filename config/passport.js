97eb4393046f68f4134fa857b2b75226c5520f94
Wells Tsai <wells.z.tsai@gmail.com>
2017-02-09 13:27:26 -0800

    Send verification link along with email and set user to active based on token with 2 day expiry

--- config/passport.js
+++ config/passport.js
@@ -23,25 +23,23 @@ module.exports = function(passport) {
     passReqToCallback: true
   },
   function(req, username, password, done) {
-   User.find({ where: {username: username}, raw: true })
+   User.find({ where: {username: username} })
     .then(function(user) {
       console.log('hits found query');
-      console.log('balhblablahbhla');
-      console.log('user:',user.password);
       if (!user) {
         console.log('hit !user');
         // return done(err);
         return done(null, false, {message: 'Invalid username.'});
         // return done(null, false, req.flash('loginMessage', 'User not found.'));
       }
-      var hash = user.password;
+      var hash = user.dataValues.password;
       if (!User.validPassword(password, hash)) {
         console.log('***Enter valid password condition***');
         // return done(err);
         return done(null, false, {message: 'Invalid password.'});
         // return done(null, false, req.flash('loginMessage', 'Invalid password.'));
       }
-      if (!user.active) {
+      if (!user.dataValues.active) {
         return done(null, false, {message: 'Please activate your account by following the instructions in the account confirmation email you received to proceed.'});
       }
       //login success
@@ -64,7 +62,7 @@ module.exports = function(passport) {
   },
   function(req, username, password, done) {
     console.log('req.body:',req.body);
-      User.find({ where: {username: username} })
+      User.find({ where: {username: username}, raw: true })
       .then(function(user) {
         if (user) {
           // return done(err);
@@ -72,9 +70,13 @@ module.exports = function(passport) {
           // return done(null, false, req.flash('signupMessage', 'That username is already taken!'));
         }
         else if (!user) {
-          var token = jwt.sign({user}, supersecret.secret, {
-              expiresIn: '90d' // expires in 90 days, unit seconds
-            });
+          // var token = jwt.sign({user}, supersecret.secret, {
+          //     expiresIn: '90d' // expires in 90 days, unit seconds
+          //   });
+          var unique = {username: req.body.username, email: req.body.email};
+          var token = jwt.sign({unique}, supersecret.secret, {
+          expiresIn: '2d' // expires in 2 days
+        });
           User.create({
             username: username,
             email: req.body.email,
